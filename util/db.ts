2982b2429f62e4de487bf18a8b184c3e87683d1d
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-09-16 22:16:02 +0200

    feat: yea wip stuff

--- util/db.ts
+++
@@ -1,2 +0,0 @@
-const reconDB = require('reconlx');
-export const db = new reconDB({uri: process.env.MONGO_URI})
--- util/statdb.ts
+++
@@ -1,2 +0,0 @@
-const reconDB = require('reconlx');
-export const statdb = new reconDB({uri: process.env.STATS_MONGO_URI})
