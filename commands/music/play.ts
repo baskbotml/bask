9e62a007925b4fdc74aae10310f0ed491b075db2
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-10-30 13:14:57 +0100

    fix: error when lacking permissions

--- commands/music/play.ts
+++ commands/music/play.ts
@@ -1,5 +1,5 @@
 import { commandModule, CommandType } from '@sern/handler';
-import { ApplicationCommandOptionType, GuildMember, GuildTextBasedChannel, VoiceBasedChannel } from 'discord.js'
+import { ApplicationCommandOptionType, GuildMember, GuildTextBasedChannel, PermissionFlagsBits, VoiceBasedChannel } from 'discord.js'
 import { distube } from '../../index.js';
 import { publish } from '../../src/plugins/publish.js';
 
@@ -17,9 +17,11 @@ export default commandModule({
         ],
 	//alias : [],
     execute: async (ctx, args) => {
+        const vcConnectionCheck = ctx.client.guilds.cache.get(ctx.guildId)?.members.cache.get(ctx.client.user!.id)?.permissionsIn((ctx.interaction.member as GuildMember).voice.channelId as string)
         if (!ctx.client.guilds.cache.get(ctx.guildId)?.members.cache.get(ctx.user.id)?.voice.channel) return await ctx.reply({content: "You are not in a voice channel!", ephemeral: true})
         // if (ctx.client.guilds.cache.get(ctx.guildId)?.members.cache.get(ctx.user.id)?.voice.channelId !== ctx.client.guilds.cache.get(ctx.guildId)?.members.cache.get(ctx.client.user?.id as string)?.voice.channel) return await ctx.reply({content: `You need to stay in the same VC as me!`, ephemeral: true})
         if (ctx.guild.members.me?.voice.channelId) {if (ctx.guild.voiceStates.cache.get(ctx.client.user!.id)?.channelId !== ctx.guild.voiceStates.cache.get(ctx.user.id)?.channelId) return await ctx.reply({content: `You need to stay in the same VC as me!`, ephemeral: true})}
+        if (!vcConnectionCheck!.has(PermissionFlagsBits.Connect) && vcConnectionCheck!.has(PermissionFlagsBits.Speak)) return await ctx.reply({content: `I can't join or speak on that VC!`, ephemeral: true})
         distube.play((ctx.interaction.member as GuildMember)?.voice.channel as VoiceBasedChannel, args[1].getString('name') as string, {
             member: ctx.interaction.member as GuildMember,
             textChannel: ctx.interaction.channel as GuildTextBasedChannel,
