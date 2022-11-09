64b12ebd2be19dd42dd209a925921eb3491770f2
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2022-11-09 14:19:41 +0100

    fix: error out when something bad happens

--- commands/music/play.ts
+++ commands/music/play.ts
@@ -17,15 +17,19 @@ export default commandModule({
         ],
 	//alias : [],
     execute: async (ctx, args) => {
-        const vcConnectionCheck = ctx.client.guilds.cache.get(ctx.guildId)?.members.cache.get(ctx.user.id)?.permissionsIn((ctx.interaction.member as GuildMember).voice.channelId as string)
-        if (!ctx.client.guilds.cache.get(ctx.guildId)?.members.cache.get(ctx.user.id)?.voice.channel) return await ctx.reply({content: "You are not in a voice channel!", ephemeral: true})
-        if (ctx.guild.members.me?.voice.channelId) {if (ctx.guild.voiceStates.cache.get(ctx.client.user!.id)?.channelId !== ctx.guild.voiceStates.cache.get(ctx.user.id)?.channelId) return await ctx.reply({content: `You need to stay in the same VC as me!`, ephemeral: true})}
-        if (!vcConnectionCheck!.has(PermissionFlagsBits.Connect)) return await ctx.reply({content: `I can't join on that VC!`, ephemeral: true})
-        if (!vcConnectionCheck!.has(PermissionFlagsBits.Speak)) return await ctx.reply({content: `I can't speak on that VC!`, ephemeral: true})
-        distube.play((ctx.interaction.member as GuildMember)?.voice.channel as VoiceBasedChannel, args[1].getString('name') as string, {
-            member: ctx.interaction.member as GuildMember,
-            textChannel: ctx.interaction.channel as GuildTextBasedChannel,
-        })
-        await ctx.reply({content: "Should be added to the queue if a message is sent now!", ephemeral: true})
+        try {
+            const vcConnectionCheck = ctx.client.guilds.cache.get(ctx.guildId)?.members.cache.get(ctx.user.id)?.permissionsIn((ctx.interaction.member as GuildMember).voice.channelId as string)
+            if (!ctx.client.guilds.cache.get(ctx.guildId)?.members.cache.get(ctx.user.id)?.voice.channel) return await ctx.reply({content: "You are not in a voice channel!", ephemeral: true})
+            if (ctx.guild.members.me?.voice.channelId) {if (ctx.guild.voiceStates.cache.get(ctx.client.user!.id)?.channelId !== ctx.guild.voiceStates.cache.get(ctx.user.id)?.channelId) return await ctx.reply({content: `You need to stay in the same VC as me!`, ephemeral: true})}
+            if (!vcConnectionCheck!.has(PermissionFlagsBits.Connect)) return await ctx.reply({content: `I can't join on that VC!`, ephemeral: true})
+            if (!vcConnectionCheck!.has(PermissionFlagsBits.Speak)) return await ctx.reply({content: `I can't speak on that VC!`, ephemeral: true})
+            distube.play((ctx.interaction.member as GuildMember)?.voice.channel as VoiceBasedChannel, args[1].getString('name') as string, {
+                member: ctx.interaction.member as GuildMember,
+                textChannel: ctx.interaction.channel as GuildTextBasedChannel,
+            })
+            await ctx.reply({content: "Should be added to the queue if a message is sent now!", ephemeral: true})
+        } catch (err) {
+            await ctx.reply({content: `Woah, something bad happened. Check if I can see or join the VC you are on.`, ephemeral: true})
+        }
 	},
 });
