0e32d12ff411cc329003d503992b8f336384f3ae
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2022-11-22 12:21:06 -0600

    refactor: refactoring

--- commands/music/play.ts
+++ commands/music/play.ts
@@ -1,5 +1,5 @@
 import { commandModule, CommandType } from '@sern/handler';
-import { ApplicationCommandOptionType, GuildMember, GuildTextBasedChannel, PermissionFlagsBits, VoiceBasedChannel } from 'discord.js'
+import { ApplicationCommandOptionType, GuildMember, GuildTextBasedChannel, PermissionFlagsBits } from 'discord.js'
 import { distube } from '../../index.js';
 import { publish } from '../../src/plugins/publish.js';
 import { inVc } from '../../src/plugins/inVc.js';
@@ -18,15 +18,16 @@ export default commandModule({
         ],
     execute: async (ctx, args) => {
         try {
-            const vcConnectionCheck = (await ctx.guild.members.fetch(ctx.user.id)).permissionsIn((ctx.member as GuildMember).voice.channelId!)
+            const currentMember = ctx.member as GuildMember;
+            const vcConnectionCheck = (await ctx.guild.members.fetch(ctx.user.id)).permissionsIn(currentMember.voice.channelId!)
             if (ctx.guild.members.me?.voice.channelId) {
                 if (ctx.guild.voiceStates.cache.get(ctx.client.user!.id)?.channelId !== ctx.guild.voiceStates.cache.get(ctx.user.id)?.channelId)
                     return ctx.reply({content: `You need to stay in the same VC as me!`, ephemeral: true})
             }
             if (vcConnectionCheck!.has(PermissionFlagsBits.Connect)) return ctx.reply({content: `I can't join on that VC!`, ephemeral: true})
             if (vcConnectionCheck!.has(PermissionFlagsBits.Speak)) return ctx.reply({content: `I can't speak on that VC!`, ephemeral: true})
-            await distube.play((ctx.member as GuildMember).voice.channel!, args[1].getString('name',true), {
-                member: ctx.member as GuildMember,
+            await distube.play(currentMember.voice.channel!, args[1].getString('name',true), {
+                member: currentMember,
                 textChannel: ctx.channel as GuildTextBasedChannel,
             })
             await ctx.reply({content: "Should be added to the queue if a message is sent now!", ephemeral: true})
